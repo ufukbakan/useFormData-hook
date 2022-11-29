0cf54fcca2c54a56380e2e7dcf0543fc0deed87c
ufukbakan <println.ufukbakan@gmail.com>
2022-11-29 10:46:21 +0300

    Example dependency update

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,16 +1,28 @@
-import React from 'react'
-import useFormData from '.';
+import React, { useRef } from 'react'
 import './App.css'
+import useFormData from "useformdata-hook";
 
 export default function App() {
 
     const formData = useFormData();
 
+    const form2ref = useRef<HTMLFormElement>(null);
+    const formData2 = useFormData({ form: form2ref.current as HTMLFormElement });
+
     return (
         <div className='grid'>
             <div className="inputs">
                 <form>
-                    <label>name <input type="text"  name='name' /></label>
+                    <label>name <input type="text" name='name' /></label>
+                    <label>birthDate <input type="date" name='birthDate' /></label>
+                    <label>favColor <input type="color" name='favouriteColor' /></label>
+                    <label>accept <input type="checkbox" defaultChecked name='accept' /></label>
+                    <label>comment <textarea name='comment'></textarea></label>
+                </form>
+
+                <form ref={form2ref}>
+                    <h2>form2</h2>
+                    <label>name <input type="text" name='name' /></label>
                     <label>birthDate <input type="date" name='birthDate' /></label>
                     <label>favColor <input type="color" name='favouriteColor' /></label>
                     <label>accept <input type="checkbox" defaultChecked name='accept' /></label>
@@ -19,7 +31,14 @@ export default function App() {
             </div>
             <div className="output">
                 <h4>useForm</h4>
-                <div>{JSON.stringify(formData)}</div>
+                <div>
+                    {JSON.stringify(formData)}
+                </div>
+
+                <h4>use secondary form</h4>
+                <div>
+                    {JSON.stringify(formData2)}
+                </div>
             </div>
         </div>
     )
